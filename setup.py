05c056b73aff8507c3c9df75e6e18103c24cc64b
luca lusvarghi <lucalusvarghi@gmail.com>
2026-02-23 16:11:52 +0100

    chore: update pyproject.toml and setup.py for package structure and metadata improvements

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="django-janus-theme",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
-    license="MIT",
-    description="A professional Enterprise Django theme.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/strikedata/jtheme",
-    author="DataStrike Team",
-    author_email="info@datastrike.it",
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 4.0",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
-    install_requires=[
-        "Django>=3.0",
-    ],
-)
+setup()
